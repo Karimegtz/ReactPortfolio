aaf88b9cdac85d723eed2556e91c9db2282d71f0
Sandra Karime Gutiérrez Martínez <sandra.gutierrez@logrand.local>
2024-08-27 12:22:06 -0600

    images pages

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as calculator } from './projects/coder.jpg';
+export { default as ledWall } from './projects/ledWall.jpeg';
+export { default as pastelPuzzels } from './projects/pastelPuzzels.png';
+export { default as runBuddy } from './projects/runBuddy.png';
+export { default as surfReport } from './projects/surfReport.jpg';
