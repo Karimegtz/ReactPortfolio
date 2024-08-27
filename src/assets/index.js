ddd7a11d9b39e9080e326132ac55c5c042d41459
Sandra Karime Gutiérrez Martínez <sandra.gutierrez@logrand.local>
2024-08-27 15:41:21 -0600

    fixing routes problems

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
-export { default as blogBuilder } from './projects/blogBuilder.jpg';
+export { default as blogBuilder } from './projects/blogBuilder.png';
 export { default as bookSearch} from './projects/bookSearch.jpg';
 export { default as codeRefactor } from './projects/codeRefactor.png';
-export { default as noteTaker } from './projects/noteTaker.png';
-export { default as textEditor } from './projects/textEditor.jpg';
+export { default as noteTaker } from './projects/noteTaker.jpg';
+export { default as textEditor } from './projects/textEditor.jpeg';
